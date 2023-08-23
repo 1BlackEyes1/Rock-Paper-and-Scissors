3d8f018bee2befc614c698f7a59b3745efc97138
pedro <blackeyes.pg@gmail.com>
2023-08-23 19:50:32 -0400

    rps-ui
    Complete

--- main.js
+++ main.js
@@ -11,103 +11,100 @@ function getComputerChoise() {
     
 }
 
+let countP = 0;
+let countC = 0;
+const countPlayer = document.createElement('p');
+document.body.appendChild(countPlayer);
+countPlayer.textContent = `PlayerScore : ${countP}`;
+
+const countComp = document.createElement('p');
+document.body.appendChild(countComp);
+countComp.textContent = `ComputerScore : ${countC}`;
+
+const btnRock = document.createElement('button')
+btnRock.textContent = 'Rock';
+document.body.appendChild(btnRock)
+
+const btnPaper = document.createElement('button');
+btnPaper.textContent = 'Paper';
+document.body.appendChild(btnPaper);
+
+const btnScissors = document.createElement('button');
+btnScissors.textContent = 'Scissors';
+document.body.appendChild(btnScissors)
+
+
+btnRock.addEventListener('click', () => {
+ playRound('Rock')
+ 
+});
+
+btnPaper.addEventListener('click', () => {
+    playRound('Paper')
+    
+});
+
+btnScissors.addEventListener('click', () => {
+    playRound('Scissors')
+})
+
+const resultPlayRound = document.createElement('div');
+document.body.appendChild(resultPlayRound)
+
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = prompt('Select your roll Rock Paper or Scissor');
-    playerSelection = playerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoise();
+   
     if(playerSelection == 'rock' && computerSelection == 'Rock'){
-        return 'Tie'
+        resultPlayRound.textContent = 'Tie';
+    } else if (playerSelection == 'rock' && computerSelection == 'Paper'){
+        resultPlayRound.textContent = 'You lose Paper beat Rock';
+        countC++
+        countComp.textContent = `ComputerScore : ${countC}`;
+    } else if (playerSelection == 'rock' && computerSelection == 'Scissors'){
+        countP++;
+        countPlayer.textContent = `PlayerScore : ${countP}`;
+        resultPlayRound.textContent = 'You win Rock beat Scissors';  
+    } else if (playerSelection == 'paper' && computerSelection == 'Paper'){
+        resultPlayRound.textContent = 'Tie';
+    } else if (playerSelection == 'paper' && computerSelection == 'Scissors'){
+        resultPlayRound.textContent = 'You lose Scissors beat Paper'
+        countC++
+        countComp.textContent = `ComputerScore : ${countC}`;
+    } else if (playerSelection == 'paper' && computerSelection == 'Rock'){
+        resultPlayRound.textContent = 'You win Paper beat Rock';
+        countP++;
+        countPlayer.textContent = `PlayerScore : ${countP}`;
+    } else if (playerSelection == 'scissors' && computerSelection == 'Scissors'){
+        resultPlayRound.textContent = 'Tie';
+    } else if (playerSelection == 'scissors' && computerSelection == 'Rock'){
+        resultPlayRound.textContent = 'You lose Rock beat Scissors';
+        countC++
+        countComp.textContent = `ComputerScore : ${countC}`;
+    } else if (playerSelection == 'scissors' && computerSelection == 'Paper'){
+    resultPlayRound.textContent = 'You win Scissors beat Paper'   
+    countP++;
+    countPlayer.textContent = `PlayerScore : ${countP}`;
     }
-    else if(playerSelection == 'rock' && computerSelection == 'Paper'){
-        return 'You lose Paper beat Rock'
-    }
-    else if(playerSelection == 'rock' && computerSelection == 'Scissors'){
-        return 'You win Rock beat Scissors'
-    }
-    else if(playerSelection == 'paper' && computerSelection == 'Paper'){
-        return 'Tie'
-    }
-    else if(playerSelection == 'paper' && computerSelection == 'Scissors'){
-        return 'You lose Scissors beat Paper'
-    }
-    else if(playerSelection == 'paper' && computerSelection == 'Rock'){
-        return 'You win Paper beat Rock'
-    }
-    else if(playerSelection == 'scissors' && computerSelection == 'Scissors'){
-        return 'Tie'
-    }
-    else if(playerSelection == 'scissors' && computerSelection == 'Rock'){
-        return 'You lose Rock beat Scissors'
-    }
-    else (playerSelection == 'scissors' && computerSelection == 'Paper')
-        return 'You win Scissors beat Paper'
 
+    if(countP == 5){
+         resultPlayRound.textContent = "congratulations you won the game"
+        }
+    else if(countC == 5){
+        resultPlayRound.textContent = "YOU LOSE! Computer Won the Game"
+        }
+
+        if(countP == 5 || countC == 5){
+            countC = 0
+            countP = 0
+            countPlayer.textContent = `PlayerScore : ${countP}`;
+            countComp.textContent = `ComputerScore : ${countC}`;
+        }
 }   
 
 
+           
 
-function game(){
-    let result
-    let playerWin = 0;
-    let computerWin = 0;
-    let x = 5;
 
-    for(let i = 0; i < x; i++){
-      result = playRound();
-      
 
-        if(result == 'You win Scissors beat Paper') {
-            playerWin += 1
-            console.log(result)
-            console.log(playerWin)
-            console.log(computerWin)
-            
-        }
-
-        else if(result == 'You win Paper beat Rock'){
-            playerWin += 1
-            console.log(result)
-            console.log(playerWin)
-            console.log(computerWin)
-            
-        }
-        else if(result == 'You win Rock beat Scissors'){
-            playerWin += 1
-            console.log(result)
-            console.log(playerWin)
-            console.log(computerWin)
-            
-        }
-        else if(result == 'You lose Rock beat Scissors'){
-            computerWin += 1 
-            console.log(result)
-            console.log(playerWin)
-            console.log(computerWin)
-         }
-         else if(result == 'You lose Scissors beat Paper'){
-            computerWin += 1 
-            console.log(result)
-            console.log(playerWin)
-            console.log(computerWin)
-         }
-         else if(result == 'You lose Paper beat Rock'){
-            computerWin += 1 
-            console.log(result)
-            console.log(playerWin)
-            console.log(computerWin)
-         }
-        else if (result == 'Tie'){
-            console.log(result)
-            x += 1
-        }
-    }
-    if(playerWin > computerWin){
-        console.log('Congratulation YOU WIN')
-    }      
-    else if (playerWin < computerWin){
-        console.log('YOU LOSE!')
-    }
-}
-
-game()
